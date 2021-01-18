294cc3f4e95d4ad58d03c6a36f795f5faccc21c0
이예은 <nina7963@naver.com>
2021-01-18 19:12:41 +0900

    앞뒤 거꾸로 읽어도 똑같은 문자열 알고리즘 코드

---
+++ Palindrome_ex.py
@@ -0,0 +1,10 @@
+def is_palindrome(word):
+    # 코드를 입력하세요.
+    for left in range(len(word)//2):
+        pass
+# 테스트
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))
