1dd71e594e1b08895992aec211f0c60aa1c2f42b
이예은 <nina7963@naver.com>
2021-01-20 13:10:23 +0900

    선택정렬 알고리즘_1

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,10 +1,10 @@
 def selectin_sort(arr):
     for i in range(len(arr)-1):
-        min_idx=i
-        for j in range(i+1, len(arr)):
+        min_idx=i #가장 작은값을 고정값으로 설정
+        for j in range(i+1, len(arr)): #작은값을 제외한 나머지 숫자와 비교
             if arr[j]<arr[min_idx]:
                 min_idx=j
-        arr[i],arr[min_idx]=arr[min_idx],arr[i] #swqp
+        arr[i],arr[min_idx]=arr[min_idx],arr[i] #swap
     return arr
 
 
